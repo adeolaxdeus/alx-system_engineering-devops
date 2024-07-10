3c49ac61a74bf585206d55d9f769537fdbec46a4
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-10 15:20:39 -0700

    fix matching case

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # find School pattern from the cmd-line argument
 input_string = ARGV[0]
-regex = /S\w{4}l/
+regex = /Sc\w{3}l/
 matches = input_string.scan(regex)
 matches.each { |match| print match }
 puts
