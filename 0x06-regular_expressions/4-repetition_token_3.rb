85c9a8575e9d6123d3dbf5b6a0b59e9974cd95dd
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-11 03:17:31 -0700

    Repetition token #3

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# match any word starting with h with b,t in btw and n ending it
+
+input_string = ARGV[0]
+regex = /^hb{0,3}t{0,5}n$/
+matches = input_string.scan(regex)
+matches.each { |match| print match }
+puts
