75198e9228b232b0ab4282c7c75a008dcb54a49b
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-11 03:27:07 -0700

    and it all on God

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -2,7 +2,7 @@
 # match any word starting with h with b,t in btw and n ending it
 
 input_string = ARGV[0]
-regex = /^hb{0,3}t{0,5}n$/
+regex = /^hb{0,3}t{0,10}n$/
 matches = input_string.scan(regex)
 matches.each { |match| print match }
 puts
