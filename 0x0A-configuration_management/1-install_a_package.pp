f11d4a5e8be5b70b24ec348a2046e25b533cee53
adeolaxdues <adeolaxdeus@gmail.com>
2024-08-11 01:49:46 -0700

    try fix checker error

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Install flask using pip3
 
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
   source   => 'https://files.pythonhosted.org/packages/16/ed/041b94d8f4e1d2c64b118bb510030c4c0f9ab4b41edc7f34b64613298f59/Flask-2.1.0.tar.gz'
