8d639ea95d8f0f32c43129f709943dff9924003c
adeolaxdues <adeolaxdeus@gmail.com>
2024-08-11 03:25:13 -0700

    replace some lines

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,13 @@
 # manifest to make changes to the configuration file
 
-file { '/etc/ssh/ssh_config':
-  ensure  => 'file',
-  mode    => '0744',
-  content => ' ',
-}
-
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentifyFile ~/.ssh/school',
-  match => '^IdentityFile',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentifyFile ~/.ssh/school',
+  replace => true,
 }
 
 file_line {'Turn off passwd Auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
-  match => '^PasswordAuthentication',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
 }
