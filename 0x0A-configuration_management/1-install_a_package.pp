67af15aa8cb0e57bcc21731f42625fb234fa87cf
adeolaxdues <adeolaxdeus@gmail.com>
2024-08-11 01:32:56 -0700

    add src attr to manifest

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,4 +3,5 @@
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+  source   => 'https://files.pythonhosted.org/packages/16/ed/041b94d8f4e1d2c64b118bb510030c4c0f9ab4b41edc7f34b64613298f59/Flask-2.1.0.tar.gz'
 }
