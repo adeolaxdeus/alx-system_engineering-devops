e8f52c8d39c510b31ec21a3e14c2f7e6590f9ab5
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-21 12:56:41 +0000

    install nginx web server {w/ puppet}

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,9 @@
+# install and configure nginx on a new ubuntu machine
+# when querying /root with a get request using curl it should,
+#  return a page contain this strong 'Hello World!'
+#  The /redirection must be a 301 moved permanently
+
+exec { 'install $ configure':
+  provider => shell,
+  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default'
+}
