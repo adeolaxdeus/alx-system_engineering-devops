d3b95442aeaab3708fd1998cc9c3278f2a621ccd
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-22 01:36:43 +0000

    remove $

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,7 @@
 #  return a page contain this strong 'Hello World!'
 #  The /redirection must be a 301 moved permanently
 
-exec { 'install $ configure':
+exec { 'install_and_configure':
   provider => shell,
-  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default; service nginx restart'
+  command  => 'sudo apt-get update; sudo apt-get -y install nginx; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html; sudo sed -i "/server_name _;/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default; service nginx restart',
 }
